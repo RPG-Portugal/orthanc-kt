239b656d2d16c45fe2d9edaf0ae66c9ede062c45
Adriano Anastácio <adriano.halo@gmail.com>
2024-10-18 15:48:17 +0100

    changed jda constructor function

--- src/main/kotlin/com/rpgportugal/orthanc/kt/Main.kt
+++ src/main/kotlin/com/rpgportugal/orthanc/kt/Main.kt
@@ -2,7 +2,9 @@ package com.rpgportugal.orthanc.kt
 
 import arrow.core.Either
 import com.rpgportugal.orthanc.kt.dependencies.Modules
+import com.rpgportugal.orthanc.kt.persistence.dto.Application
 import com.rpgportugal.orthanc.kt.persistence.repository.application.ApplicationRepository
+import dev.minn.jda.ktx.jdabuilder.createJDA
 import dev.minn.jda.ktx.jdabuilder.light
 import net.dv8tion.jda.api.requests.GatewayIntent
 import org.koin.core.context.startKoin
@@ -14,16 +16,16 @@ fun main() {
 
     val koin = startKoin { modules(Modules.modules) }.koin
 
-    val applicationRepository =
-        koin.get<ApplicationRepository>()
-
-    val application =
+    val applicationRepository = koin.get<ApplicationRepository>()
+    
+    val application: Application =
         when(val result = applicationRepository.getApplicationById(1L)) {
             is Either.Right -> result.value
             is Either.Left -> throw Exception(result.value.message)
         }
 
-    val jda = light(token=application.token, enableCoroutines = true) {
-        setEnabledIntents(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
-    }
+    val jda = createJDA(
+        token = application.token,
+        enableCoroutines = true,
+        intents = GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
 }
