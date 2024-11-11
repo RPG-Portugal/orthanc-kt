86c7dfeca1f5c566ae608f33ae08c722a0c89c29
Adriano Anastácio <adriano.halo@gmail.com>
2024-11-11 12:19:22 +0000

    cleaned code

--- src/main/kotlin/com/rpgportugal/orthanc/kt/dependencies/DbModule.kt
+++ src/main/kotlin/com/rpgportugal/orthanc/kt/dependencies/DbModule.kt
@@ -17,7 +17,6 @@ import com.rpgportugal.orthanc.kt.persistence.repository.job.db.SqlJobRepository
 import jakarta.persistence.EntityManager
 import org.hibernate.SessionFactory
 import org.hibernate.cfg.Configuration
-import org.hibernate.community.dialect.SQLiteDialect
 import org.hibernate.hikaricp.internal.HikariCPConnectionProvider
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -25,39 +24,41 @@ import org.koin.dsl.module
 object DbModule : DepModule, Logging {
 
     override val module = module {
-        single {
-            val properties = when (val either = get<PropertiesLoader>().load("app.properties")) {
-                is Either.Right -> either.value
-                is Either.Left -> {
-                    log.error("Failed to load app.properties: {}", either.value.message)
-                    throw Exception("Failed to load app.properties")
-                }
-            }
-
-            properties.run {
-                setProperty(
-                    "hibernate.connection.provider_class",
-                    HikariCPConnectionProvider::class.java.name
-                )
-                setProperty(
-                    "hibernate.hbm2ddl.auto",
-                    "update"
-                )
-            }
-
-            Configuration()
-                .addProperties(properties)
-                .addAnnotatedClass(Application::class.java)
-                .addAnnotatedClass(Emoji::class.java)
-                .addAnnotatedClass(JobConfiguration::class.java)
-                .addAnnotatedClass(RoleAwardConfiguration::class.java)
-                .buildSessionFactory()
-                ?: throw Exception("Unable to build session factory")
-
-        } bind SessionFactory::class
+        single { buildSessionFactory(get()) } bind SessionFactory::class
         factory { get<SessionFactory>().createEntityManager() } bind EntityManager::class
         factory { SqlApplicationRepository(get()) } bind ApplicationRepository::class
         factory { SqlEmojiRepository(get()) } bind EmojiRepository::class
         factory { SqlJobRepository(get()) } bind JobRepository::class
+    }
+
+    private fun buildSessionFactory(propertiesLoader: PropertiesLoader): SessionFactory {
+        val properties = when (val either = propertiesLoader.load("app.properties")) {
+            is Either.Right -> either.value
+            is Either.Left -> {
+                log.error("Failed to load app.properties: {}", either.value.message)
+                throw Exception("Failed to load app.properties ${either.value.message}")
+            }
+        }
+
+        properties.run {
+            setProperty(
+                "hibernate.connection.provider_class",
+                HikariCPConnectionProvider::class.java.name
+            )
+            setProperty(
+                "hibernate.hbm2ddl.auto",
+                "update"
+            )
+        }
+
+        return Configuration()
+            .addProperties(properties)
+            .addAnnotatedClass(Application::class.java)
+            .addAnnotatedClass(Emoji::class.java)
+            .addAnnotatedClass(JobConfiguration::class.java)
+            .addAnnotatedClass(RoleAwardConfiguration::class.java)
+            .buildSessionFactory()
+            ?: throw Exception("Unable to build session factory")
+
     }
 }
